3f3abb4cdfd186102058d22284bb70164060b5bb
KinjalThakar <thakarkinjal1992@gmail.com>
2022-06-14 17:42:50 +0530

    jquery,greylayout

---
+++ Day6/CSS Practice/js/index.js
@@ -0,0 +1,26 @@
+$(document).ready(function()
+{
+	// alert("document is ready");
+
+	// $("#hide").click(function(){
+
+	// 	$("#details").hide();
+	// });
+
+	// $("#show").click(function(){
+
+	// 	$("#details").show();
+		// $("#Toggle").click(function(){
+
+	 // 	$("#details").toggle();
+
+// $("#Toggle").click(function(){
+
+// 	 	$("#details").fadeToggle(1000);
+	$("#Toggle").click(function(){
+
+	 	$("#details").fadeToggle("slow");
+
+
+		});
+});
